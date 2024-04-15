8572a0b5fa079313501ff36f0a199dbc6a2c4389
Anurag Majumdar <lightningphoenix484@gmail.com>
2024-04-15 11:47:01 +0530

    Server Port Updated

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import Order from "./Models/Order.js";
 
 dotenv.config();
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(cors());
